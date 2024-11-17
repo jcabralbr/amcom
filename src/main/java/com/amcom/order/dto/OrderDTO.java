adf8f3fd2a29d9d065bfe0a439997e095893dd32
Julio Cesar Cabral <julio.cabral@brmalls.com.br>
2024-11-16 21:33:19 -0300

    refatorar para usar DTOs

--- src/main/java/com/amcom/order/dto/OrderDTO.java
+++ src/main/java/com/amcom/order/dto/OrderDTO.java
@@ -2,14 +2,10 @@ package com.amcom.order.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 @Builder
-@Getter
-@Setter
 @JsonSerialize
 public record OrderDTO(String orderId, List<OrderItemDTO> items) {
 }
--- src/main/java/com/amcom/order/dto/OrderItemDTO.java
+++ src/main/java/com/amcom/order/dto/OrderItemDTO.java
@@ -2,14 +2,10 @@ package com.amcom.order.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Builder
-@Getter
-@Setter
 @JsonSerialize
 public record OrderItemDTO(String sku, int quantity, BigDecimal unitPrice) {
 }
